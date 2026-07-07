bfd8a7f55bcac5d9373e9d0a96dad2465b8f9bf0
jeffersonRBatistaV <jebatistav14@gmail.com>
2026-07-06 23:04:54 -0400

    setup GitHub Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://jeffersonrbatistav.github.io',
+  base: '/imperioW/',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
